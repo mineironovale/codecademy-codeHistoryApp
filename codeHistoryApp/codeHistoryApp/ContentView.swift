fae4788abfc4e8a8aaebafc54e4b2b5c4a429724
Augusto Messias <augustomessias@pm.me>
2022-04-16 18:12:00 -0300

    Adicionar cor principal à ZStack

--- codeHistoryApp/codeHistoryApp/ContentView.swift
+++ codeHistoryApp/codeHistoryApp/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
     let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        ZStack{}
+        ZStack{
+            mainColor.ignoresSafeArea()
+        }
     }
 }
 
