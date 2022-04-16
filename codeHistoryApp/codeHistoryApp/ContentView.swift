0d03176c8f101ad15938e6228d16a0a6236d9aa8
Augusto Messias <augustomessias@pm.me>
2022-04-16 18:00:41 -0300

    Adicionar constantes de cor

--- codeHistoryApp/codeHistoryApp/ContentView.swift
+++ codeHistoryApp/codeHistoryApp/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
         Text("Hello, world!")
             .padding()
